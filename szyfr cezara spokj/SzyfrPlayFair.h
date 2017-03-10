1d410d87112437d7f1caed2ddd51ab777b1a98b6
DESKTOP-L6SJCDI\Marcin <mar.czekaj@o2.pl>
2017-03-10 15:36:20 +0100

    Szyfr Playfaira

---
+++ szyfr cezara spokj/SzyfrPlayFair.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class SzyfrPlayFair
+{
+    string napis,napisPoprawny,tekst;
+    bool czyMoznaSzyfrowac,czyMoznaDeszyfrowac;
+    char tablica[5][5];
+    int licznik,licznik2,liczbaElementow;
+    char litera;
+public:
+    SzyfrPlayFair(string);
+
+    ~SzyfrPlayFair();
+
+    void Szyfruj();
+
+    void Deszyfruj();
+
+    void SwitchPlayFaira();
+
+    void PodajNapis();
+
+};
